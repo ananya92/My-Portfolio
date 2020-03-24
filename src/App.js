16109b89273cdab725fd588f6db541d68cf1585e
ananya92 <1992ananya@gmail.com>
2020-03-24 13:20:56 +1030

    Installed react-mdl to use its layout. Created the basic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import { Layout, Header, Drawer, Navigation, Content} from 'react-mdl';
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <div style={{height: '100vh', position: 'relative'}}>
+      <Layout style={{background: 'url(/images/cover.jpg) center / cover'}}>
+          <Header transparent title="My Portfolio">
+              <Navigation>
+                  <a href="/">About Me</a>
+                  <a href="/">Projects</a>
+                  <a href="/">Resume</a>
+                  <a href="/">Contact</a>
+              </Navigation>
+          </Header>
+          <Drawer title="My Portfolio">
+              <Navigation>
+                  <a href="/">About Me</a>
+                  <a href="/">Projects</a>
+                  <a href="/">Resume</a>
+                  <a href="/">Contact</a>
+              </Navigation>
+          </Drawer>
+          <Content />
+      </Layout>
+  </div>
   );
 }
 
