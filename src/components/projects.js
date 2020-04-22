f738300e2af8221110e4ec3524f879c47467e8ef
ananya92 <1992ananya@gmail.com>
2020-04-23 00:34:29 +0930

    Coded projects page to import and read the project list and create a projectCard component for each project in project list

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap';
+import Footer from "./footer";
+import projectList from "../utils/projectList";
+import ProjectCard from "./projectCard";
 class Projects extends Component {
     render() {
         return (
             <div>
-                <h1>Projects Page</h1>
-            </div>
+            <Container>
+                <Row>
+                    <h1 className="headerStyle landingGrid">Projects</h1>
+                </Row>
+                <Row>
+                    {projectList.map((project) => (
+                        <Col className="projectCard" xs={12} sm={6} md={4}>
+                            <ProjectCard project = {project}></ProjectCard>
+                        </Col>
+                    ))}
+                </Row>
+                </Container>
+                <div className="push"></div>
+                <Footer></Footer>
+                </div>
         )
     }
 }
